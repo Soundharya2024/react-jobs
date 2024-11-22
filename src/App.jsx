51d9a78bd262d8e0c41b4e6449d5602ab3d53eb9
Soundharya S <soundharya@ecappz.in>
2024-11-22 22:13:16 +0530

    Adding a new 'Jobs' page with data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage from "./pages/JobPage";
+import jobLoader from "./functions/jobloader";
+import AddJobPage from "./pages/AddJobPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route path="/react-jobs" element={<HomePage />} />
+      <Route path="/react-jobs/jobs" element={<JobsPage />} />
+      <Route path="/react-jobs/add-job" element={<AddJobPage />} />
+      <Route
+        path="/react-jobs/jobs/:id"
+        element={<JobPage />}
+        loader={jobLoader}
+      />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 export default function App() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +7,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-  }
-})
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
